1c8cd256da8435df98463495f8d99b9cb2335462
octaq <octa.quinteroo@gmail.com>
2024-04-18 21:06:33 -0300

    provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Asegúrate de importar desde 'react-dom/client'
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store/store';
+import { store, persistor } from './redux/store/store'; // Asegúrate de que estos son los nombres correctos
 import App from './App';
+import { PersistGate } from 'redux-persist/integration/react'; // Asegúrate de tener esta importación si usas PersistGate
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
--- src/redux/store/store.jsx
+++ src/redux/store/store.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-import rootReducer from '../reducer/reducer'; // Importa el reducer combinado
+import storage from 'redux-persist/lib/storage';
+import rootReducer from '../reducer/reducer';
 
 const persistConfig = {
   key: 'root',
@@ -12,6 +12,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE', 'persist/REGISTER'],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
